527c9d8fe6e2852eea063ce9c8110c63da95b20d
MariaKir-coder <55013626+MariaKir-coder@users.noreply.github.com>
2019-11-08 12:37:09 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main //Variant 1 Kireeva Maria
-{
-    public static void main(String[] args)
-    {
-        //Task1 newtask1= new Task1();
-        Task2 newtask2= new Task2();
-    }
-}
